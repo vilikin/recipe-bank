f06872bac06b119bac99de3bdec8eb52c2c6016f
chibieiko <8eikochan@gmail.com>
2021-01-06 19:31:25 +0200

    Use styled-components

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,37 +1,65 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
 import Amplify from "aws-amplify";
 import { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
+import styled from "styled-components";
 
 Amplify.configure({
   Auth: {
     region: "eu-west-1",
     userPoolId: "eu-west-1_Uf5yzuFsm",
     userPoolWebClientId: "5hifnhh26nphmm5l2ej38b2fit",
-    mandatorySignIn: true
-  }
+    mandatorySignIn: true,
+  },
 });
+
+const AppContainer = styled.div`
+  text-align: center;
+`;
+
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const AppLink = styled.a`
+  color: #61dafb;
+`;
+
+const AppLogo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+  animation: App-logo-spin infinite 20s linear;
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <AppContainer>
+      <AppHeader>
+        <AppLogo src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <AppLink href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
-        </a>
-      </header>
+        </AppLink>
+      </AppHeader>
       <AmplifySignOut />
-    </div>
+    </AppContainer>
   );
 }
 
